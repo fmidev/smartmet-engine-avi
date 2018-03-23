f188acd612b418c3b4a092fe1429b901b17f8006
pjukka <pjukka@users.noreply.github.com>
2018-03-23 10:17:04 +0200

    Test return types of Config class accessors.

--- test/config.cpp
+++ test/config.cpp
@@ -27,16 +27,41 @@ BOOST_AUTO_TEST_CASE(config_accessors,
 {
   const std::string filename = "cnf/valid.conf";
   Config config(filename);
+  const std::string stringVariable;
+  const int intVariable = 0;
+  const bool boolVariable = true;
+
+  BOOST_CHECK(typeid(config.getHost()) == typeid(stringVariable));
   BOOST_CHECK_EQUAL(config.getHost(), "smartmet-test");
+
+  BOOST_CHECK(typeid(config.getPort()) == typeid(intVariable));
   BOOST_CHECK_EQUAL(config.getPort(), 5444);
+
+  BOOST_CHECK(typeid(config.getDatabase()) == typeid(stringVariable));
   BOOST_CHECK_EQUAL(config.getDatabase(), "avi");
+
+  BOOST_CHECK(typeid(config.getUsername()) == typeid(stringVariable));
   BOOST_CHECK_EQUAL(config.getUsername(), "avi_user");
+
+  BOOST_CHECK(typeid(config.getPassword()) == typeid(stringVariable));
   BOOST_CHECK_EQUAL(config.getPassword(), "avi_pw");
+
+  BOOST_CHECK(typeid(config.getEncoding()) == typeid(stringVariable));
   BOOST_CHECK_EQUAL(config.getEncoding(), "UTF8");
+
+  BOOST_CHECK(typeid(config.getMaxMessageStations()) == typeid(intVariable));
   BOOST_CHECK_EQUAL(config.getMaxMessageStations(), 0);
+
+  BOOST_CHECK(typeid(config.getMaxMessageRows()) == typeid(intVariable));
   BOOST_CHECK_EQUAL(config.getMaxMessageRows(), 0);
+
+  BOOST_CHECK(typeid(config.getRecordSetStartTimeOffsetHours()) == typeid(intVariable));
   BOOST_CHECK_EQUAL(config.getRecordSetStartTimeOffsetHours(), 30);
+
+  BOOST_CHECK(typeid(config.getRecordSetEndTimeOffsetHours()) == typeid(intVariable));
   BOOST_CHECK_EQUAL(config.getRecordSetEndTimeOffsetHours(), 12);
+
+  BOOST_CHECK(typeid(config.getFilterFIMETARxxx()) == typeid(boolVariable));
   BOOST_CHECK_EQUAL(config.getFilterFIMETARxxx(), true);
 
   const std::list<std::string> filterFIMETARxxxExcludeIcaosEmpty;
