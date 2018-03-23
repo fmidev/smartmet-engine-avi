891c986affb822be63aa05543c19601b6e79caba
pjukka <pjukka@users.noreply.github.com>
2018-03-23 16:07:28 +0200

    Add unit tests for the accessors of Connection class.

--- test/connection.cpp
+++ test/connection.cpp
@@ -34,7 +34,46 @@ BOOST_AUTO_TEST_CASE(connection_constructor_valid_args)
   BOOST_CHECK(connection.isConnected());
 }
 
-//  BOOST_CHECK_EQUAL(messageTypes.size(), 1);
+BOOST_AUTO_TEST_CASE(connection_accessors)
+{
+  const bool boolVariable = true;
+  const std::string stringVariable;
+  const std::string filename = "cnf/valid.conf";
+  Config conf(filename);
+  Connection connection(conf.getHost(),
+                        conf.getPort(),
+                        conf.getUsername(),
+                        conf.getPassword(),
+                        conf.getDatabase(),
+                        conf.getEncoding());
+  BOOST_CHECK(typeid(connection.collateSupported()) == typeid(boolVariable));
+  BOOST_CHECK(not connection.collateSupported());
+
+  BOOST_CHECK(typeid(connection.quote(filename)) == typeid(stringVariable));
+  BOOST_CHECK_EQUAL(connection.quote(filename), std::string("'").append(filename).append("'"));
+}
+
+BOOST_AUTO_TEST_CASE(connection_query_select_age)
+{
+  const std::string filename = "cnf/valid.conf";
+  Config conf(filename);
+  Connection connection(conf.getHost(),
+                        conf.getPort(),
+                        conf.getUsername(),
+                        conf.getPassword(),
+                        conf.getDatabase(),
+                        conf.getEncoding());
+  connection.setDebug(true);
+
+  const std::string selectAge("SELECT AGE(timestamp '2001-04-10', timestamp '1957-06-13');");
+  pqxx::result selectAgeResult = connection.executeNonTransaction(selectAge);
+  BOOST_CHECK_EQUAL(selectAgeResult.query(), selectAge);
+  BOOST_CHECK(selectAgeResult.size() > 0);
+
+  auto resultTuple = selectAgeResult.at(0);
+  BOOST_CHECK_EQUAL(resultTuple["age"].c_str(), "43 years 9 mons 27 days");
+}
+
 }  // namespace Avi
 }  // namespace Engine
 }  // namespace SmartMet
